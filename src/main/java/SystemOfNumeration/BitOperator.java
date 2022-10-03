8e868572aa544fa923a3b4b52ef733cbad1e6b55
turbogym <gymgym@88.com>
2022-10-04 02:49:20 +0800

    update

---
+++ src/main/java/SystemOfNumeration/BitOperator.java
@@ -0,0 +1,18 @@
+package SystemOfNumeration;
+
+public class BitOperator {
+    public static void main(String[] args) {
+        // 1向右移2位↓
+        int a = 1 >> 2;
+        int b = -1 >> 2;
+        int c = 1 << 2;
+        int d = -1 << 2;
+        // 算数右移↓
+        int e = 3 >>> 2;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+    }
+}
