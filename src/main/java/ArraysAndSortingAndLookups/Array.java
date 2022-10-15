a8883b303fac521087dcc490a84999347bc56cce
turbogym <gymgym@88.com>
2022-10-15 17:30:47 +0800

    update_array

--- src/main/java/ArraysAndSortingAndLookups/Array.java
+++ src/main/java/ArraysAndSortingAndLookups/Array.java
@@ -73,7 +73,7 @@ public class Array {
          * 1) 数组是多个相同类型数据的组合，实现对这些数据的统一管理。
          * 2) 数组中的元素可以是任何数据类型，包括基本类型和引用类型，但是不能混用。
          * 3) 数组创建后，如果没有赋值，有默认值：
-         *    int -> 0, short -> 0, byte -> 0, long 0, float -> 0.0, double -> 0.0, char -> \u0000
+         *    int -> 0, short -> 0, byte -> 0, long -> 0, float -> 0.0, double -> 0.0, char -> \u0000
          *    boolean -> false, String -> null
          * 4) 使用数组的步骤 1.声明数组并开辟空间 2.给数组各个元素赋值 3.使用数组
          * 5) 数组的下标是从0开始的。
