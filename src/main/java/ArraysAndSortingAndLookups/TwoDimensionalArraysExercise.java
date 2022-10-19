fdc94b619b44c03db7b6c80b49dbae9e8f2a7c77
turbogym <gymgym@88.com>
2022-10-19 23:20:07 +0800

    :sparkles:update_Array

--- src/main/java/ArraysAndSortingAndLookups/TwoDimensionalArraysExercise.java
+++ src/main/java/ArraysAndSortingAndLookups/TwoDimensionalArraysExercise.java
@@ -54,5 +54,52 @@ public class TwoDimensionalArraysExercise {
         for (int i = 0; i < arr2.length; i++) {
             System.out.print(arr2[i] + "\t");
         }
+
+        /**
+         * 随机生成10个整数（1~100范围内）保存到数组，并倒序打印以及求平均值、求最大值和最大值的下标、并查找里面是否有8
+         */
+        int[] arr3 = new int[10];
+        int sum2 = 0;
+        double avg = 0;
+        int max = 0;
+        int maxIndex = -1;
+        for (int i = 0; i < 10; i++) {
+            arr3[i] = (int) (Math.random() * 100 + 1);
+            sum2 += arr3[i];
+            if (arr3[i] > max) {
+                max = arr3[i];
+                maxIndex = i;
+            }
+        }
+        System.out.print("\n未排序的数组为：");
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.print(arr3[i] + "\t");
+        }
+        System.out.print("\n未排序的数组最大值为：" + max + "，下标为：" + maxIndex);
+
+        for (int i = 0; i < arr3.length; i++) {
+            if (8 == arr3[i]) {
+                System.out.print("\n当前下标" + i + "元素为8");
+            }
+        }
+        System.out.print("\n倒序打印的数组：");
+        for (int i = arr3.length - 1; i >= 0; i--) {
+            System.out.print(arr3[i] + "\t");
+        }
+        for (int i = 0; i < arr3.length - 1; i++) {
+            for (int j = 0; j < arr3.length - i - 1; j++) {
+                if (arr3[j] < arr3[j + 1]) {
+                    int temp = arr3[j];
+                    arr3[j] = arr3[j + 1];
+                    arr3[j + 1] = temp;
+                }
+            }
+        }
+        System.out.print("\n降序打印数组：");
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.print(arr3[i] + "\t");
+        }
+        avg = sum2 / (double) arr3.length;
+        System.out.println("\nsum = " + sum2 + " ,avg = " + avg);
     }
 }
