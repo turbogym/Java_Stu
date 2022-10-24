71e9abc2b5356d299349e04db1b822fb7769ac43
turbogym <gymgym@88.com>
2022-10-24 16:52:28 +0800

    VarParameter

---
+++ src/main/java/ObjectOrientedProgramming/VarParameterExercise.java
@@ -0,0 +1,28 @@
+package ObjectOrientedProgramming;
+
+import com.sun.istack.internal.NotNull;
+
+import java.util.Scanner;
+
+public class VarParameterExercise {
+    /**
+     * 有三个方法，分别实现返回姓名和两门课成绩（总分），返回姓名和三门课成绩（总分），返回姓名和五门课成绩（总分）。
+     * 封装成一个可变参数的方法
+     * 类名HspMethodEx  方法名showScore
+     */
+    public static void main(String[] args) {
+        HspMethodEx hspMethodEx = new HspMethodEx();
+        System.out.println(hspMethodEx.showScore("Jack", 80.5, 60.5));
+        System.out.println(hspMethodEx.showScore("Terry", 90, 78.5, 89));
+    }
+}
+
+class HspMethodEx {
+    public String showScore(String name, double... scores) {
+        double totalScore = 0;
+        for (int i = 0; i < scores.length; i++) {
+            totalScore += scores[i];
+        }
+        return name + "的" + scores.length + "门课总分为：" + totalScore;
+    }
+}
