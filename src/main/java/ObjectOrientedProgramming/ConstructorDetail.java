1e8fb57146a3351068902b971a49ee20a579af99
turbogym <gymgym@88.com>
2022-10-25 23:34:21 +0800

    This

--- src/main/java/ObjectOrientedProgramming/ConstructorDetail.java
+++ src/main/java/ObjectOrientedProgramming/ConstructorDetail.java
@@ -13,8 +13,26 @@ public class ConstructorDetail {
      *      比如 Person () {}，使用javap指令 反编译查看
      * 7) 一旦定义了自己的构造器，默认的构造器就覆盖了，就不能再使用默认的无参构造器，除非显示的定义一下，即：Person (){}
      */
-}
 
-class Dog {
-
+    /**
+     * 对象创建的流程分析
+     * 案例：
+     * class Person {
+     *     int age = 90;
+     *     String name;
+     *
+     *     Person(String n, int a) {
+     *         name = n;
+     *         age = a;
+     *     }
+     * }
+     * Person p = new Person("小倩", 20);
+     * 分析：
+     * 在内存中，首先在方法区加载Person类（Person.class）。
+     * new的时候现在 堆 中开辟一个空间，堆 中这个空间首先对age和name进行默认的初始化，对应的0和null。
+     * 然后进行显示的初始化，由于int age = 90，所以 堆 中的age的0就变为了90。
+     * 接着看到new Person("小倩", 20);中还有构造器的处理，"小倩"是存在 常量池 的，所以 堆 中的age变为20，name指向 常量池 的一个地址。
+     * 最后才把 堆 中的这一块空间地址返回给 栈 中的 p。
+     * 注：真正的 对象 是 堆 中的那一块空间，p是对象的引用。
+     */
 }
